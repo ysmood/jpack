34d601982e022715c28d1ca9a0883a469063a653
Yad Smood <y.s.inside@gmail.com>
2015-05-25 00:32:43 +0800

    add: schema generator

--- src/jpack.js
+++ src/jpack.js
@@ -3,6 +3,57 @@
     var Jpack = function Jpack () {
     }
 
+    function getType (val) {
+        var type = typeof val
+        switch (type) {
+        case 'object':
+            if (val === null) {
+                return 'null'
+            } else if (val.constructor === Array) {
+                return 'array'
+            } else {
+                return 'object'
+            }
+        default:
+            return type
+        }
+    }
+
+    /**
+     * Generate simple schema from a sample object. Use it
+     * if you feel boring to craft a schema by hand.
+     * @param  {Object} obj
+     * @return {Object}
+     */
+    Jpack.prototype.genSchema = function (obj) {
+        var schema = {}
+
+        var iter = function (node, schema) {
+            var type = getType(node)
+
+            schema.type = type
+
+            switch (type) {
+            case 'array':
+                schema.items = {}
+                iter(node[0], schema.items)
+                break
+
+            case 'object':
+                schema.properties = {}
+                for (key in node) {
+                    schema.properties[key] = {}
+                    iter(node[key], schema.properties[key])
+                }
+                break
+            }
+        }
+
+        iter(obj, schema)
+
+        return schema
+    }
+
     var jpack = new Jpack
 
     // CMD & AMD Support
--- test/lab.js
+++ test/lab.js
@@ -1,2 +1,43 @@
+var kit = require('nokit')
 var jpack = require('../src/jpack')
 
+var sample1 = {
+    id: 10,
+    color: 'red',
+    tags: [
+        'sun', 'warm'
+    ]
+}
+
+var sample2 = [1, 2]
+
+var schema1 = {
+    type: 'objet',
+    properties: {
+        id: {
+            type: 'number'
+        },
+        color: {
+            type: 'string'
+        },
+        tags: {
+            type: 'array',
+            items: {
+                type: 'string'
+            }
+        }
+    }
+}
+
+var schema2 = {
+    type: 'array',
+    items: {
+        type: 'number'
+    }
+}
+
+kit.log(JSON.stringify(
+    jpack.genSchema(sample1),
+    null,
+    4
+))
