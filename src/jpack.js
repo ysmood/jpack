3b3463d74adb878c99d5b76e7480614afd288d82
Yad Smood <y.s.inside@gmail.com>
2015-05-27 00:36:26 +0800

    opt

--- src/jpack.js
+++ src/jpack.js
@@ -1,174 +1,216 @@
 ;(function (root) {
     var Jpack = function Jpack () {
-    }
+        self = this
 
-    /**
-     * Generate simple schema from a sample object. Use it
-     * if you feel boring to craft a schema by hand.
-     * @param  {Object} obj
-     * @return {Object}
-     */
-    Jpack.prototype.genSchema = function (obj) {
-        var schema = schemaIter(obj, {})
+        /**************************** Public *************************************/
 
-        schema.hash = hash(schema)
-
-        return schema
-    }
-
-    /**
-     * Serialize anything to a data pack.
-     * @param  {Any} obj
-     * @param  {Object} schema
-     * @return {Any}
-     */
-    Jpack.prototype.pack = function (val, schema) {
-        return packIter(val, schema, [])
-    }
-
-    /**
-     * Deserialize the data pack to the origin value.
-     * @param  {jpack} data
-     * @param  {Object} schema
-     * @return {Any}
-     */
-    Jpack.prototype.unpack = function (data, schema) {
-        return unpackIter(data, schema)
-    }
-
-    function schemaIter (node, schema) {
-        schema.type = getType(node)
-
-        switch (schema.type) {
-        case 'array':
-            schema.items = {}
-            schemaIter(node[0], schema.items)
-            break
-
-        case 'object':
-            schema.properties = {}
-            for (key in node) {
-                if (!node.hasOwnProperty(key))
-                    continue
-
-                schema.properties[key] = {}
-                schemaIter(node[key], schema.properties[key])
+        /**
+         * User can use it to extend the data type that jpack supports.
+         * @type {Object} Each type should implement two
+         * functions. One is `serialize`: `(val) ->  String | Number | Boolean | Null`.
+         * Another one is `parse`: `(val) -> any`.
+         * @example
+         * This will let jpack support `Date` type.
+         * ```javascript
+         * jpack.types['Date'] = {
+         *     serialize: function (val) {
+         *         return val.getTime()
+         *     },
+         *     parse: function (val) {
+         *         return new Date(val)
+         *     }
+         * }
+         * ```
+         */
+        self.types = {
+            'Date': {
+                serialize: function (val) {
+                    return val.getTime()
+                },
+                parse: function (val) {
+                    return new Date(val)
+                }
             }
-            break
         }
 
-        return schema
-    }
+        /**
+         * Generate simple schema from a sample object. Use it
+         * if you feel boring to craft a schema by hand.
+         * @param  {Object} obj
+         * @return {Object}
+         */
+        self.genSchema = function (obj) {
+            var schema = schemaIter(obj, {})
 
-    function getType (val) {
-        var type = typeof val
-        switch (type) {
-        case 'object':
-            if (val === null) {
-                return 'null'
-            } else if (val instanceof Array) {
-                return 'array'
-            } else if (val instanceof Date) {
-                return 'date'
-            } else {
+            schema.hash = hash(schema)
+
+            return schema
+        }
+
+        /**
+         * Serialize anything to a data pack.
+         * @param  {Any} obj
+         * @param  {Object} schema
+         * @return {Any}
+         */
+        self.pack = function (val, schema) {
+            return packIter(val, schema, [])
+        }
+
+        /**
+         * Deserialize the data pack to the origin value.
+         * @param  {jpack} data
+         * @param  {Object} schema
+         * @return {Any}
+         */
+        self.unpack = function (data, schema) {
+            return unpackIter(data, schema)
+        }
+
+        /**************************** Private ***************************************/
+
+        function schemaIter (node, schema) {
+            schema.type = getType(node)
+
+            switch (schema.type) {
+            case 'array':
+                schema.items = {}
+                schemaIter(node[0], schema.items)
+                break
+
+            case 'object':
+                schema.properties = {}
+                for (key in node) {
+                    if (!node.hasOwnProperty(key))
+                        continue
+
+                    schema.properties[key] = {}
+                    schemaIter(node[key], schema.properties[key])
+                }
+                break
+            }
+
+            return schema
+        }
+
+        function getType (val) {
+            var type = typeof val
+            switch (type) {
+            case 'object':
+                if (val === null) return 'null'
+
+                type = val.constructor.name
+
+                if (type == 'Array') return 'array'
+
+                if (self.types[type]) return type
+
                 return 'object'
+
+            default:
+                return type
             }
-        default:
-            return type
-        }
-    }
-
-    function hash (obj) {
-        var str = JSON.stringify(obj)
-
-        var l = 16
-        var h = Math.pow(2, ((len - len % 2) / 2))
-        var rollLen = l - 1
-        var mask = 0xffffffff >>> (32 - l)
-        var len = str.length
-
-        for (var i = 0; i < len; i++) {
-            h = ( (h << 1 | h >>> rollLen) & mask ) ^ str.charCodeAt(i)
-        }
-        return h >>> 0
-    }
-
-    function packIter (node, schema, arr) {
-        switch (schema.type) {
-        case 'array':
-            for (var i = 0; i < node.length; i++) {
-                arr.push(
-                    // Every item should be same typed,
-                    // so we only have to get the first one's type.
-                    packIter(node[i], schema.items, [])
-                )
-            }
-            break
-
-        case 'object':
-            for (key in schema.properties) {
-                arr.push(
-                    packIter(node[key], schema.properties[key], [])
-                )
-            }
-            break
-
-        case 'date':
-            return node.getTime()
-
-        default:
-            return node
         }
 
-        return arr
-    }
+        function hash (obj) {
+            var str = JSON.stringify(obj)
 
-    function unpackIter (node, schema) {
-        switch (schema.type) {
-        case 'array':
-            var obj = []
-            for (var i = 0; i < node.length; i++) {
-                obj.push(
-                    unpackIter(node[i], schema.items)
-                )
+            var l = 16
+            var h = Math.pow(2, ((len - len % 2) / 2))
+            var rollLen = l - 1
+            var mask = 0xffffffff >>> (32 - l)
+            var len = str.length
+
+            for (var i = 0; i < len; i++) {
+                h = ( (h << 1 | h >>> rollLen) & mask ) ^ str.charCodeAt(i)
             }
-            break
-
-        case 'object':
-            var i = 0,
-                obj = {}
-            for (key in schema.properties) {
-                obj[key] =
-                    unpackIter(node[i++], schema.properties[key])
-            }
-            break
-
-        case 'date':
-            return new Date(node)
-
-        default:
-            return node
+            return h >>> 0
         }
 
-        return obj
-    }
+        function packIter (node, schema, arr) {
+            var type = schema.type
 
-    /**
-     * Encoding an array into a binary buffer.
-     * @param  {Array} arr It only contains
-     * `Array`, `String`, `Number`, `Boolean` and `null`.
-     * @return {Uint8Array}
-     */
-    function encoding (arr) {
+            switch (type) {
+            case 'array':
+                for (var i = 0; i < node.length; i++) {
+                    arr.push(
+                        // Every item should be same typed,
+                        // so we only have to get the first one's type.
+                        packIter(node[i], schema.items, [])
+                    )
+                }
+                break
 
-    }
+            case 'object':
+                for (key in schema.properties) {
+                    arr.push(
+                        packIter(node[key], schema.properties[key], [])
+                    )
+                }
+                break
 
-    /**
-     * Decoding a buffer to an array.
-     * @param  {Uint8Array} buf
-     * @return {Array}
-     */
-    function decoding (buf) {
+            default:
+                var handler = self.types[type]
+                if (handler)
+                    return handler.serialize(node)
+
+                return node
+            }
+
+            return arr
+        }
+
+        function unpackIter (node, schema) {
+            var type = schema.type
+
+            switch (schema.type) {
+            case 'array':
+                var obj = []
+                for (var i = 0; i < node.length; i++) {
+                    obj.push(
+                        unpackIter(node[i], schema.items)
+                    )
+                }
+                break
+
+            case 'object':
+                var i = 0,
+                    obj = {}
+                for (key in schema.properties) {
+                    obj[key] =
+                        unpackIter(node[i++], schema.properties[key])
+                }
+                break
+
+            default:
+                var handler = self.types[type]
+                if (handler)
+                    return handler.parse(node)
+
+                return node
+            }
+
+            return obj
+        }
+
+        /**
+         * Encoding an array into a binary buffer.
+         * @param  {Array} arr It only contains
+         * `Array`, `String`, `Number`, `Boolean` and `null`.
+         * @return {Uint8Array}
+         */
+        function encoding (arr) {
+
+        }
+
+        /**
+         * Decoding a buffer to an array.
+         * @param  {Uint8Array} buf
+         * @return {Array}
+         */
+        function decoding (buf) {
+        }
+
     }
 
     var jpack = new Jpack
